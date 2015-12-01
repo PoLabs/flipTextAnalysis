f3ed26f8ffcd97a71f5b9a016d0cfec81fb965b7
Chris Facer <chris.facer@q-researchsoftware.com>
2015-12-01 14:51:09 +1100

    Consolidating R files

--- R/flipTextAnalytics-utilities.R
+++ R/flipTextAnalytics-utilities.R
@@ -131,3 +131,17 @@ countUniqueTokens = function(tokenized) {
   tokens = sort(tokens)
   return(list(tokens = tokens, counts = counts))
 }
+
+#' @export
+print.tidyText = function(x, ...) 
+{
+    dd = data.frame("Original Text" = names(x), "Transformed Text" = as.character(x))
+    print(DT::datatable(dd))
+}
+
+# Phrases are represented with '+' joining the words,
+# but we don't want to print these out for the user.
+makeWordBagTextReadable = function(text) 
+{
+  return(gsub("[+]", " ", text))
+}
