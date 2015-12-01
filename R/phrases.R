0576d73c047ca87bcc0ec92c5b04cf9ca8e2b69a
Chris Facer <chris.facer@q-researchsoftware.com>
2015-12-01 14:44:15 +1100

    Consolidated phrase code

--- R/phrases.R
+++ R/phrases.R
@@ -65,3 +65,28 @@ GetMostFrequentPhrases <- function(text, num.words = c(2,3), min.frequency = 3)
   counts <- counts[counts > min.frequency]
   return(data.frame(phrases = tokens, counts = counts))
 }
+
+# This function adds a delimiter to any phrases found in the text.
+# In this framework when we want to treat a sequence of words as a
+# single unit we join them with "+" so that they don't get split
+# by the tokenizer, which uses whitespace. Given a character vector
+# of phases, this function iterates through the character vector
+# of text and adds the delimiter.
+replacePhrasesInText <- function(text, phrases)
+{
+  .convertPhrasesToTagged <- function(phrases) 
+  {
+      split.phrases <- ftaTokenize(phrases)
+      tagged.phrases <- sapply(split.phrases, paste, collapse = "+")
+      return(tagged.phrases)
+  }
+
+  tagged.phrases <- .convertPhrasesToTagged(phrases)
+  for (j in 1L:length(phrases))
+  {
+    text <- gsub(phrases[j], tagged.phrases[j], text)
+  }
+  return(text)  
+}
+
+  
